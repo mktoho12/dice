885a36651131927405d7a07cfac502473193c05d
mktoho12 <makoto_16n1200102@nnn.ed.jp>
2018-08-27 18:30:55 +0900

    first commit

---
+++ config.ru
@@ -0,0 +1,3 @@
+require './dice'
+run App
+
---
+++ dice.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+class App < Sinatra::Base
+  get '/' do
+    slim :index
+  end
+
+  get %r{/(\d+)d(\d+)} do |dice, face|
+    eyes = roll(dice.to_i, face.to_i)
+    @source = "#{dice}d#{face}"
+    @sum = eyes.inject(&:+)
+    slim :dice
+  end
+
+  helpers do
+    def roll(dice, face)
+      ([nil] * dice).map{ rand(face) + 1 }
+    end
+  end
+end
